7026c74d6f0baf06a300a7c9d42ad23284d89590
Valya <valya.ponomarenko.00@mail.ru>
2023-05-16 17:13:12 +0300

    Финальная версия

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include "win.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Win win(0); // создание объекта управления окном
+       win.show(); // визуплизация окна
 
     return a.exec();
 }
